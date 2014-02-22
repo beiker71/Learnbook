3ae3f5a65a1a487fd0fc29be72d6e2b697005060
Ellen Meiselman <ellenm1@gmail.com>
2014-02-22 09:39:26 -0500

    cleaned up page array

--- js-local/pageArray.js
+++ js-local/pageArray.js
@@ -1,34 +1,41 @@
 /***IMPORTANT***
 	The page array generator (http://mlearning.med.umich.edu/quiz/cbtlib/modules/common/js/pa/) should be used to 
 	create your page array. */
+	
 var PageArray = new Array(  
-{buttonTitle:'Competency Criteria', title:' ', url:'page01.htm', chapter:0,level:1 },
-{buttonTitle:'Content Pages', title:' ', url:'page02.htm', chapter:1,level:1 },
-{buttonTitle:'Attestation Statement', title:'', url:'attestation.htm', chapter:2, level:1, type:'I', quiz:'409666286641', rm:'', countscore:'1'},
-{buttonTitle:'Score &amp; Status Page', title:'',url:'scorePage.htm',chapter:6,level:1 }
+{buttonTitle:'0 Intro Competency Criteria', title:' ', url:'page01.htm', chapter:0,level:1 },
+{buttonTitle:'Qualtrics Quiz', title:'', url:'qualtricsWrap.htm?href=https://qualtrics.com/SE/?SID=SV_3ggggggQOfTcqxf', chapter:1, level:1, type:'U', quiz:'392025628426', qmax:2, rm:'', countscore:'1'},
+{buttonTitle:'1 Content', title:' ', url:'page02.2.htm', chapter:2,level:1 },
+{buttonTitle:'2 Content', title:' ', url:'page04.htm', chapter:3,level:1 },
+{buttonTitle:'3 Quiz', title:'', url:'quizWrap.htm?call=embed&session=1140870896389821&href=http://myquestionmarkserver.com/perception5/session.php', chapter:4, level:1, type:'Q', quiz:'1140870896389821', rm:'', countscore:'1'},
+{buttonTitle:'4 Content', title:' ', url:'page08.htm', chapter:5,level:1 },
+{buttonTitle:'5 Content', title:' ', url:'TOC8.htm', chapter:5,level:1 },
+{buttonTitle:'6 Content', title:' ', url:'page02.1.htm', chapter:1,level:1 },
+{buttonTitle:'Captivate Quiz', title:'', url:'captivate6Wrap.htm?swf=captivate/scenario1a.swf&w=640&h=480', chapter:2, level:2, type:'C', quiz:'435692636224', rm:'', countscore:'1'},
+{buttonTitle:'Qualtrics Quiz', title:'', url:'qualtricsWrap.htm?href=https://qualtrics.com/SE/?SID=SV_6hggggg3jEHN1Qh', chapter:2, level:2, type:'U', quiz:'980284740118', qmax:1, rm:'', countscore:'1'},
+{buttonTitle:'Qualtrics Quiz', title:'', url:'captivate6Wrap.htm?swf=captivate/scenario1b.swf&w=640&h=480', chapter:2, level:2, type:'C', quiz:'478983681194', rm:'', countscore:'1'},
+{buttonTitle:'7 Score &amp; Status Page', title:'',url:'scorePage.htm',chapter:6,level:1 }
 //IMPORTANT!!! the last item does NOT get a comma at the end.
 ); 	
 	
-var moduletype=8;
+var moduletype=2;
 
 var docTitle=('MLearning SCORM-compatible HTML template');
 var headerTitle=('MLearning SCORM-compatible HTML template');
-
-
+var contentExpert=new Array( {email:"jerrysmith@example.com",name:"Jerry Smith"}, {email:"janesmith@example.com",name:"Jane Smith"}, {email:"joesmith@example.com",name:"Joe Smith"} );
+ 
 var recommendedMsg = 'Recommended';
 var requiredMsg = 'Must be completed to finish module';
 var completedMsg   = 'Completed';
 
-var IntArray = new Object();
-IntArray['p01_000'] =  {id:'p01_000', tries:0, ascore:0, amax:1, req:1, msg:'', status:0, quiz:'469585915910'},
-IntArray['p01_001'] =  {id:'p01_001', tries:0, ascore:0, amax:1, req:2, msg:'', status:0, quiz:'469585915910'},
-IntArray['p03_001'] =  {id:'p03_001', tries:0, ascore:0, amax:1, req:1, msg:'', status:0, quiz:'409666286641'},
-IntArray['p05_000'] =  {id:'p05_000', tries:0, ascore:0, amax:1, req:1, msg:'', status:0, quiz:'5679585915911'},
-IntArray['p05_001'] =  {id:'p05_001', tries:0, ascore:0, amax:1, req:2, msg:'', status:0, quiz:'5679585915911'}
+//var IntArray = new Object();
+//IntArray['p01_000'] =  {id:'p01_000', tries:0, ascore:0, amax:1, req:1, msg:'', status:0, quiz:'469585915910'},
+//IntArray['p01_001'] =  {id:'p01_001', tries:0, ascore:0, amax:1, req:2, msg:'', status:0, quiz:'469585915910'},
+////IntArray['p02_001'] =  {id:'p02_001', tries:0, ascore:0, amax:1, req:1, msg:'', status:0, quiz:'409666286655'},
+//IntArray['p03_000'] =  {id:'p03_000', tries:0, ascore:0, amax:1, req:1, msg:'', status:0, quiz:'409666286641'},
+//IntArray['p03_001'] =  {id:'p03_001', tries:0, ascore:0, amax:1, req:1, msg:'', status:0, quiz:'409666286641'}
+//IntArray['p05_000'] =  {id:'p05_000', tries:0, ascore:0, amax:1, req:1, msg:'', status:0, quiz:'5679585915911'},
+//IntArray['p05_001'] =  {id:'p05_001', tries:0, ascore:0, amax:1, req:2, msg:'', status:0, quiz:'5679585915911'}
 
 
-//IMPORTANT!!! the last item does NOT get a comma at the end.
-); 
-
-var docTitle=('MLearning SCORM-compatible HTML template');
-var headerTitle=('MLearning SCORM-compatible HTML template');
+ 
